4bed2457d47065f88e0e67f28d0e4de8997baa0f
James Summers <jamesnsummers@gmail.com>
2017-06-16 13:35:40 -0500

    adding structure

---
+++ timeline-app/src/components/Header.js
@@ -0,0 +1,15 @@
+// src/components/Header.js
+import React, {Component} from 'react'
+import {Link} from 'react-router'
+
+class Header extends Component{
+  render(){
+    return (
+      <header>
+        <h1>Hello World</h1>
+      </header>
+    )
+  }
+}
+
+export default Header
--- timeline-app/src/index.js
+++ timeline-app/src/index.js
@@ -1,8 +1,11 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import {Router, browserHistory} from 'react-router'
+import routes from './config/routes.js'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('root')
+);
