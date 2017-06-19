76a0dccfbad1add52ff73d354582e174eff4dff0
James Summers <jamesnsummers@gmail.com>
2017-06-18 21:31:14 -0500

    wrote code in firebase.js and installed firebase

--- timeline-app/src/utils/firebase.js
+++ timeline-app/src/utils/firebase.js
@@ -1,1 +1,41 @@
 // TODO: Use .dotenv
+import firebase from 'firebase';
+
+// Provided by the Firebase console
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "946980286390"
+};
+
+// Firebase instance
+firebase.initializeApp(config);
+
+// Firebase doesn't return data as an array but as a parent object
+//  containing child objects. This utility function will extract
+//  the child objects into an array and place the key as 'id'
+const firebaseListToArray = (firebaseObjectList) => {
+  if (!firebaseObjectList) return [];
+
+  return Object.keys(firebaseObjectList)
+    .map(k => {
+      const obj = {
+        id: k
+      };
+      for (let key in firebaseObjectList[k]) {
+        if (firebaseObjectList[k].hasOwnProperty(key)) {
+          obj[key] = firebaseObjectList[k][key];
+        }
+      }
+      return obj;
+    });
+}
+
+const database = firebase.database();
+const auth = firebase.auth();
+
+export { firebase, database, auth };
+export { firebaseListToArray };
