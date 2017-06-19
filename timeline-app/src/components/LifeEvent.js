cf252d56444ce98c8efadf009dc9ab98e06377c1
James Summers <jamesnsummers@gmail.com>
2017-06-19 15:48:14 -0500

    made post and show work on FE for DB entries

--- timeline-app/src/components/LifeEvent.js
+++ timeline-app/src/components/LifeEvent.js
@@ -6,12 +6,10 @@ class LifeEvent extends Component {
   render() {
     return (
       <div className="row lifeEvent">
-        <div className="col-md-1 col-sm-1 lifeEvent-profile">
-          <img className="lifeEvent-profile-pic" src={ this.props.title } alt="" />
-        </div>
         <div className="col-md-11 col-sm-11 lifeEvent-text">
           <blockquote>
-            <p className="lifeEvent-text">{ this.props.data.content }</p>
+            <p className="lifeEvent-title">{ this.props.data.title }</p>
+            <p className="lifeEvent-content">{ this.props.data.content }</p>
           </blockquote>
         </div>
       </div>
