1fb4e75d9e0f8b9b90aad4a15fddf24003088a1b
James Summers <jamesnsummers@gmail.com>
2017-06-21 19:57:37 -0500

    added all fields to EditLifeEventForm

--- timeline-app/src/components/EditLifeEventForm.js
+++ timeline-app/src/components/EditLifeEventForm.js
@@ -7,51 +7,112 @@ class EditLifeEventForm extends Component {
     super(props)
     // set initial state
     this.state = {
-        title: '',
-        content: ''
+      eventDate: '',
+      postDate: '',
+      title: '',
+      isPublic: false,
+      content: '',
+      tags: '',
+      photo: '',
+      userRating: '',
+      uid: ''
     }
   }
-  onTitleChange(e){
-      this.setState({
-        title: e.target.value
-      })
 
-  }
-  onContentChange(e){
-    this.setState({
-      content: e.target.value
-    })
-  }
   onFormSubmit(e){
-
     e.preventDefault()
     let updatedLifeEvent = {
+      eventDate: this.state.eventDate || this.props.lifeEvent.eventDate,
+      postDate: this.state.postDate || this.props.lifeEvent.postDate,
       title: this.state.title || this.props.lifeEvent.title,
-      content: this.state.content || this.props.lifeEvent.content
+      isPublic: this.state.isPublic || this.props.lifeEvent.isPublic,
+      content: this.state.content || this.props.lifeEvent.content,
+      tags: this.state.tags || this.props.lifeEvent.tags,
+      photo: this.state.photo || this.props.lifeEvent.photo,
+      userRating: this.state.userRating || this.props.lifeEvent.userRating,
     }
     this.props.onUpdateLifeEvent(updatedLifeEvent, this.props.lifeEvent._id)
 
     this.setState({
+      eventDate: '',
+      postDate: '',
       title: '',
-      content: ''
+      isPublic: false,
+      content: '',
+      tags: '',
+      photo: '',
+      userRating: '',
+      uid: ''
     })
   }
   render(){
     return (
       <div className='editLifeEventForm' data-lifeEvents-index={this.props.lifeEvent._id}>
-        <form onSubmit={event => this.onFormSubmit(event)}>
-          <textarea
-            onChange={event => this.onTitleChange(event)}
-            placeholder='Update Title...'
-            type='text'
-            value={this.state.title} /> {'  '}
-          <textarea
-            onChange={event => this.onContentChange(event)}
-            placeholder='Update Content...'
-            type='text'
-            value={this.state.content} />{'  '}
-            <br />
-          <button className="btn btn-primary" type='submit'>Update!</button>
+        <form onSubmit={ e => this.onFormSubmit(e) } className="form-add-lifeEvent">
+          <div className="row">
+            <input
+              onChange={ e => { this.setState({ eventDate: e.target.value }) } }
+              value={ this.state.eventDate }
+              className="form-control"
+              rows="3"
+              placeholder="Edit the event date..." />
+          </div>
+
+          <div className="row">
+            <input
+              onChange={ e => { this.setState({ title: e.target.value }) } }
+              value={ this.state.title }
+              className="form-control"
+              rows="3"
+              placeholder="Edit the title..." />
+          </div>
+
+          <div className="row">
+            <input
+              onChange={ e => { this.setState({ content: e.target.value }) } }
+              value={ this.state.content }
+              className="form-control"
+              rows="3"
+              placeholder="Edit what happened..." />
+          </div>
+
+          <div className="row">
+            <input
+              onChange={ e => { this.setState({ tags: e.target.value }) } }
+              value={ this.state.tags }
+              className="form-control"
+              rows="3"
+              placeholder="Edit the tags..." />
+          </div>
+
+          <div className="row">
+            <input
+              onChange={ e => { this.setState({ photo: e.target.value }) } }
+              value={ this.state.photo }
+              className="form-control"
+              rows="3"
+              placeholder="Upload a different photo" />
+          </div>
+
+          <div className="row">
+            <input
+              onChange={ e => { this.setState({ userRating: e.target.value }) } }
+              value={ this.state.userRating }
+              className="form-control"
+              rows="3"
+              placeholder="Change the rating..." />
+          </div>
+
+          <div className="row">
+            <input
+              type="checkbox"
+              value={ this.state.isPublic }
+              /> {' '} Public?
+            </div>
+
+          <div className="row">
+            <button className="btn btn-primary">Update!</button>
+          </div>
         </form>
       </div>
     )
